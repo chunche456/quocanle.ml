08987367d221089a96c14514e00eb4689e27b63c
quocanle <lequocan2004@gmail.com>
2021-09-30 22:52:19 +0700

    22:52 30/09/2021

--- video/justdisappear/index.php
+++ video/justdisappear/index.php
@@ -3,7 +3,7 @@ $name = "どうせ無くなるだけ _ たかやん (Official Music Video)";
 $poster = '"/poster\justdisappear.jpg"';
 $url_video = '"https://6zvtrcldmkkwj4ko5cwamq-on.drv.tw/share/video/justdisappear.mp4"';
 //$definition = '<source label="4K" src="https://6zvtrcldmkkwj4ko5cwamq-on.drv.tw/share/video/nhumi.mkv" type="video/mp4">';
-$subtitles = '<track label="Vietnamese" kind="subtitles" srclang="en" src="subtitles/vtt/justdisappear.vtt" default>';
+$subtitles = '<track label="Vietnamese" kind="subtitles" srclang="en" src="subtitles_video/vtt/justdisappear.vtt" default>';
 $description_p = 'Đây là music video どうせ無くなるだけ _ たかやん (Official Music Video)';
 include($_SERVER['DOCUMENT_ROOT'].'/video/head_video.php');
 include($_SERVER['DOCUMENT_ROOT'].'/chongcopy.html');
