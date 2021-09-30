6a0820b0e5898ed29efd1fb5c4a756e494a000b6
quocanle <lequocan2004@gmail.com>
2021-09-30 22:28:54 +0700

    22:28 30/09/2021

--- video/lilac/index.php
+++ video/lilac/index.php
@@ -1,7 +1,7 @@
 <?php
 $name = "[MV] IU(아이유)_LILAC(라일락)";
 $poster = '"/poster\lilac.jpg"';
-$url_video = '"https://6zvtrcldmkkwj4ko5cwamq-on.drv.tw/share/video/lialc.mp4"';
+$url_video = '"https://6zvtrcldmkkwj4ko5cwamq-on.drv.tw/share/video/lilac.mp4"';
 // $subtitles = '<track label="English" kind="subtitles" srclang="en" src="captions/vtt/sintel-en.vtt" default>';
 $description_p = 'Đây là music video [MV] IU(아이유)_LILAC(라일락)';
 include($_SERVER['DOCUMENT_ROOT'].'/video/head_video.php');
